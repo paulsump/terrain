79ec44445e55ab47e280c1f52d97cc02700982b1
paulsump <sumpner@hotmail.com>
2022-03-19 17:41:28 +0000

    generate one triangleStrip . longer and still unit.

--- lib/model/generate.dart
+++ lib/model/generate.dart
@@ -10,26 +10,21 @@ final noWarn = [out];
 
 /// See README.md
 /// This function is the interface to this file
-Mesh generateMesh() => generateTriangleStripMesh(2);
+Mesh generateMesh() => generateTriangleStripMesh(4);
 
 Mesh generateTriangleStripMesh(int n) {
   final vertices = <Vector3>[];
 
   for (int i = 0; i < n; ++i) {
-    final y = i.toDouble();
+    final y = i / n;
 
     vertices.add(Vector3(0, y, 0));
-    vertices.add(Vector3(1, y, 0));
+    vertices.add(Vector3(1 / n, y, 0));
   }
-  // final vertices= [
-  //    Vector3(0, 0, 0),
-  //    Vector3(1, 0, 0),
-  //    Vector3(0, 1, 0),
-  //    Vector3(1, 1, 0),
-  //  ];
 
   return Mesh(
     vertices: vertices,
-    normals: List<Vector3>.generate(vertices.length, (i) => Vector3(0, 0, 1)),
+    normals: List<Vector3>.generate(
+        vertices.length, (i) => Vector3(0, 0, i / vertices.length)),
   );
 }
