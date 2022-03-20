59f034171ed5af0659e2c5709c0b87d3d6355b3f
paulsump <sumpner@hotmail.com>
2022-03-20 09:48:55 +0000

    added MeshBuilder class.

--- lib/model/generate.dart
+++ lib/model/generate.dart
@@ -6,61 +6,69 @@ import 'package:terrain/model/mesh.dart';
 import 'package:terrain/out.dart';
 import 'package:vector_math/vector_math_64.dart';
 
-final noWarn = [out, _rotateX];
+final noWarn = [
+  out,
+  _rotateX,
+];
 
 /// See README.md
 /// This function is the interface to this file
 Mesh generateMesh() {
-  final mesh = generateTriangleStripMesh(3);
-  _rotateX(75, mesh);
+  final builder = MeshBuilder(3);
+  final mesh = builder.getMesh();
 
+  _rotateX(75, mesh);
   return mesh;
 }
 
-void _perturbe(Mesh mesh) {}
-
-void _calcFaceNormals(Mesh mesh) {}
-
-void _calcVertexNormals(Mesh mesh) {}
-
-// n = num quads
-Mesh generateTriangleStripMesh(int n) {
+///generateTriangleStripMesh
+class MeshBuilder {
   final vertices = <Vector3>[];
 
   final indices = <int>[];
-  final step = 1 / n;
 
-  for (int X = 0; X <= n; ++X) {
-    for (int Y = 0; Y <= n; ++Y) {
-      final x = X * step;
-      final y = Y * step;
+  // void _perturbHeights() {}
+  // void _calcFaceNormals() {}
+  // void _calcVertexNormals() {}
 
-      vertices.add(Vector3(x, y, 0));
-      if (X != n && Y != n) {
-        final N = n + 1;
+// n = num quads
+  MeshBuilder(int n) {
+    final step = 1 / n;
 
-        final leftDiagonal = Y + 1 + X * N;
-        final rightDiagonal = X * N + N + Y;
+    for (int X = 0; X <= n; ++X) {
+      for (int Y = 0; Y <= n; ++Y) {
+        final x = X * step;
+        final y = Y * step;
 
-        indices.add(rightDiagonal);
-        indices.add(leftDiagonal);
-        indices.add(X * N + Y);
+        vertices.add(Vector3(x, y, 0));
+        if (X != n && Y != n) {
+          final N = n + 1;
 
-        indices.add(leftDiagonal);
-        indices.add(rightDiagonal);
-        indices.add(X * N + 1 + N + Y);
+          final leftDiagonal = Y + 1 + X * N;
+          final rightDiagonal = X * N + N + Y;
+
+          indices.add(rightDiagonal);
+          indices.add(leftDiagonal);
+          indices.add(X * N + Y);
+
+          indices.add(leftDiagonal);
+          indices.add(rightDiagonal);
+          indices.add(X * N + 1 + N + Y);
+        }
       }
     }
   }
 
-  return Mesh(
-    vertices: vertices,
-    normals: List<Vector3>.generate(
-      vertices.length,
-      (i) => Vector3(0, 0, 1),
-    ),
-    indices: indices,
-  );
+  Mesh getMesh() {
+    return Mesh(
+      vertices: vertices,
+      normals: List<Vector3>.generate(
+        vertices.length,
+        (i) => Vector3(0, 0, 1),
+      ),
+      indices: indices,
+    );
+  }
 }
 
 void _rotateX(double degrees, Mesh mesh) {
